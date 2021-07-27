b488bbd1661867c15fdae5c764d43190fdba5359
savokin <locovname@gmail.com>
2021-07-27 09:00:48 -0400

    ref and setup

--- vue3/src/components/Main.vue
+++ vue3/src/components/Main.vue
@@ -1,15 +1,21 @@
 <template>
   <div>
-      <bruh>bruh</bruh>
+      <bruh>bruh level : {{b_lev}}</bruh>
   </div>
 </template>
 
 <script>
+import { ref } from "vue";
 export default {
+    setup(){
+        const b_lev = ref('bruh');
+        return { b_lev };
+    }
 
 }
 </script>
-
+    
+  
 <style>
 
 </style>
