025f6770bb21b358e436c01dcda6681f8fbd1b42
savokin <locovname@gmail.com>
2021-08-04 08:31:59 -0400

    welcome to legend of cory in the house and the dark steam dragon

--- vue3/src/components/Second.vue
+++ vue3/src/components/Second.vue
@@ -2,7 +2,8 @@
   <div>
       <p> click the button to change {{num}} to summin else </p>
       <button v-on:click="num += 1">Button</button>
-      <p>This is a computed() property {{comp}}  (jajajaja)</p>
+      <p>This is a computed() property {{comp}} </p>
+      <p>open console to see watchEffect() in action</p>
   </div>
 </template>
 
