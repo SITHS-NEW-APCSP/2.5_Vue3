1a171a73165723f911d9809068c5af8ff4671eb9
savokin <locovname@gmail.com>
2021-08-04 08:28:53 -0400

    oh yeah

--- vue3/src/components/Second.vue
+++ vue3/src/components/Second.vue
@@ -2,7 +2,7 @@
   <div>
       <p> click the button to change {{num}} to summin else </p>
       <button v-on:click="num += 1">Button</button>
-      <p>This is a computed() property {{comp}}  (jajajaja)</p>
+      <p>This is a computed() property {{comp}} </p>
   </div>
 </template>
 
